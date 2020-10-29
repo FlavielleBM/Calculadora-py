44b37c1c9a22b8e8772453f6a17ea439bf5516cf
joice cardoso <joice.cardoso@dbserver.com.br>
2020-10-28 22:51:38 -0300

    feat: Adiciona calculo de divisao, testes das operacoes de soma e divisao

--- src/calculadora/models.py
+++ src/calculadora/models.py
@@ -3,6 +3,8 @@ class Calculadora:
         self._valor = valor
     def somar(self, valor):
         self._valor += valor
+    def dividir(self,valor):
+        self._valor /= valor
     @property
     def valor(self):
         return self._valor
--- test/calculadora/test_calculadora.py
+++ test/calculadora/test_calculadora.py
@@ -4,3 +4,19 @@ class TestCalculadora(TestCase):
     def test_deveInformarOValorInicialParaCalculadora(self):
         calculadora = Calculadora(10)
         self.assertEqual(10, calculadora.valor)
+    
+    def test_deveExecutarUmaSoma(self):
+        calculadora = Calculadora(10)
+        calculadora.somar(20)
+        self.assertEqual(30, calculadora.valor)
+
+    def test_deveExecutarUmaDivisao(self):
+        calculadora = Calculadora(10)
+        calculadora.dividir(20)
+        self.assertEqual(0.5, calculadora.valor)
+
+    def test_deveExecutarUmaDivisaoComResultadoDaSoma(self):
+        calculadora = Calculadora(10)
+        calculadora.somar(20)
+        calculadora.dividir(20)
+        self.assertEqual(1.5, calculadora.valor)
