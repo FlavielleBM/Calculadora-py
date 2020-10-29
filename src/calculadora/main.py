2bd9dc435888d90723e8634d0de0996a1980d8a0
joice cardoso <joice.cardoso@dbserver.com.br>
2020-10-29 10:18:42 -0300

    Resolvendo conflito main

--- src/calculadora/main.py
+++ src/calculadora/main.py
@@ -4,12 +4,10 @@ calculadora = Calculadora(10)
 
 calculadora.somar(20)
 print(f'Valor após cálculo de soma: {calculadora.valor}')
-<<<<<<< HEAD
 
 calculadora.limpar()
 print(f'Valor após limpar: {calculadora.valor}')
-=======
+
 calculadora.dividir(20)
 print(f'Valor após cálculo de divisao: {calculadora.valor}')
 
->>>>>>> origin/develop
